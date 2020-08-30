1ade6e54bc4bed1b688ea4be72904c38fcba0c31
KMDean <51332613+KMDean@users.noreply.github.com>
2020-08-30 15:09:38 -0400

    Update mail.php
    Testing contact page

--- mail.php
+++ mail.php
@@ -1,36 +1,16 @@
 <?php
-if(isset( $_POST['name']))
-$name = $_POST['name'];
-if(isset( $_POST['email']))
-$email = $_POST['email'];
-if(isset( $_POST['message']))
-$message = $_POST['message'];
-if(isset( $_POST['subject']))
-$subject = $_POST['subject'];
-if ($name === ''){
-echo "Name cannot be empty.";
-die();
+if(!empty($_POST["send"])) {
+	$name = $_POST["userName"];
+	$email = $_POST["userEmail"];
+	$subject = $_POST["subject"];
+	$content = $_POST["content"];
+
+	$toEmail = "kaitlyndean9@gmail.com";
+	$mailHeaders = "From: " . $name . "<". $email .">\r\n";
+	if(mail($toEmail, $subject, $content, $mailHeaders)) {
+	    $message = "Your contact information is received successfully.";
+	    $type = "success";
+	}
 }
-if ($email === ''){
-echo "Email cannot be empty.";
-die();
-} else {
-if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
-echo "Email format invalid.";
-die();
-}
-}
-if ($subject === ''){
-echo "Subject cannot be empty.";
-die();
-}
-if ($message === ''){
-echo "Message cannot be empty.";
-die();
-}
-$content="From: $name \nEmail: $email \nMessage: $message";
-$recipient = "kaitlyndean9@gmail.com";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $content, $mailheader) or die("Error!");
-echo "Email sent!";
+require_once "contact.php";
 ?>
